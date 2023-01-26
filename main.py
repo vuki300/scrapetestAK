f4e9675d3aa021740dbf59b29fec5298bf125bbf
vuki <vuk3003@Gmail.com>
2023-01-26 20:04:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import pandas as pd
 import csv
 import requests
 from bs4 import BeautifulSoup
+
+user_agent = {'User-agent': 'Mozilla/5.0'}
+url = 'https://www.alka.co.rs/monitori-najbolja-cena-led-tft-lcd.html'
+
+def get_page_contents(url):
+    page = requests.get(url, headers = user_agent)
+    return BeautifulSoup(page.text, 'html.parser')
+
+soup = get_page_contents(url)
+
+products = []
+for links in soup.find_all('h1', class_='naziv_proizvoda'):
+     product = links.get_text().strip()
+     products.append(product)
+print(products)
+
+pricesM = []
+for links in soup.find_all('span', class_='cifra malaCena'):
+    priceM = links.get_text().strip()
+    pricesM.append(priceM)
+print(pricesM)
