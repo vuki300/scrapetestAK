46491c2464908990938b6a7e30f2bb9017c57e53
vuki <vuk3003@Gmail.com>
2023-01-26 20:29:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 
 user_agent = {'User-agent': 'Mozilla/5.0'}
-url = 'https://www.alka.co.rs/monitori-najbolja-cena-led-tft-lcd.html'
+url = 'https://www.alka.co.rs/jbl.html'
 
 def get_page_contents(url):
     page = requests.get(url, headers = user_agent)
     return BeautifulSoup(page.text, 'html.parser')
 
 soup = get_page_contents(url)
+
+#SCRAPE
 
 products = []
 for links in soup.find_all('h1', class_='naziv_proizvoda'):
@@ -23,3 +25,25 @@ for links in soup.find_all('span', class_='cifra malaCena'):
     priceM = links.get_text().strip()
     pricesM.append(priceM)
 print(pricesM)
+
+pricesK = []
+for links in soup.find_all('span', class_='cifra velikaCena'):
+    priceK = links.get_text().strip()
+    pricesK.append(priceK)
+print(pricesK)
+
+descs = []
+for links in soup.find_all('span', class_='naslov'):
+    desc = links.get_text().strip()
+    descs.append(desc)
+print(descs)
+
+
+#Dict
+
+list_dict = {'Product': products, 'PriceMP': pricesM, 'PriceVP': pricesK}
+print(len(products), len(pricesM), len(pricesK))
+
+game = pd.DataFrame.from_dict(list_dict, orient='index')
+games = game.transpose()
+games.head(3)
